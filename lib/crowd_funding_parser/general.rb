a6cb0531988fd8906e4e5c77e327e88f3cab7d1b
Stan Low <a22301613@yahoo.com.tw>
2015-01-15 17:01:09 +0800

    create single interface get_result for get doc or json, and basic taobao's get result done

--- lib/crowd_funding_parser/general.rb
+++ lib/crowd_funding_parser/general.rb
@@ -1,29 +1,29 @@
 module CrowdFundingParser
   module Parser
     class General
-      def parse_tracking_data(doc, project_url)
+      def parse_tracking_data(result, project_url)
         project = Hash.new
-        project['money_goal']      = get_money_goal(doc).to_i
-        project['money_pledged']   = get_money_pledged(doc).to_i
-        project['backer_count']    = get_backer_count(doc).to_i
-        project['last_time']       = get_last_time(doc)
+        project['money_goal']      = get_money_goal(result).to_i
+        project['money_pledged']   = get_money_pledged(result).to_i
+        project['backer_count']    = get_backer_count(result).to_i
+        project['last_time']       = get_last_time(result)
         project['status']          = get_status(project['last_time'])
         # project['backer_list']     = get_backer_list(project_url)
-        project['fb_count']        = get_fb_count(doc).to_i
-        project['following_count'] = get_following_count(doc).to_i
+        project['fb_count']        = get_fb_count(result).to_i
+        project['following_count'] = get_following_count(result).to_i
         project
       end
 
-      def parse_content_data(doc, project_url)
+      def parse_content_data(result, project_url)
         project                  = Hash.new
         project['platform_project_id'] = get_id(project_url)
-        project['title']         = get_title(doc)
+        project['title']         = get_title(result)
         project['url']           = project_url
-        project['summary']       = get_summary(doc)
-        project['category']      = get_category(doc)
-        project['creator_name']  = get_creator_name(doc)
-        project['creator_id']    = get_creator_id(doc)
-        project['creator_link']  = get_creator_link(doc)
+        project['summary']       = get_summary(result)
+        project['category']      = get_category(result)
+        project['creator_name']  = get_creator_name(result)
+        project['creator_id']    = get_creator_id(result)
+        project['creator_link']  = get_creator_link(result)
         project
       end
 
@@ -51,9 +51,24 @@ module CrowdFundingParser
         links
       end
 
+      def get_result(project_url)
+        if @parse_method == :json
+          project_id = get_id(project_url)
+          project_api = get_project_api(project_id)
+          get_json_through_url(project_api)
+        else
+          get_doc_through_url(project_url)
+        end
+      end
+
       def get_doc_through_url(project_url)
         project_html = open(project_url)
         Nokogiri::HTML(project_html)
+      end
+
+      def get_json_through_url(project_url)
+        open_url = open(project_url)
+        json = JSON.load(open_url)
       end
 
       private
@@ -61,6 +76,10 @@ module CrowdFundingParser
       def get_id(project_url)
         rel_url = get_rel_url(project_url)
         rel_url.split("/").last
+      end
+
+      def summarize_project_content(string)
+        string.to_s[0..500].strip
       end
 
       def get_rel_url(url)
--- lib/crowd_funding_parser/parser/taobao.rb
+++ lib/crowd_funding_parser/parser/taobao.rb
@@ -1,17 +1,20 @@
 require "json"
 require 'open-uri'
+require 'watir'
 
 module CrowdFundingParser
   module Parser
     class Taobao < General
       def initialize
+        @parse_method = :json
         # status: 成功 = 2, 募資中 = 1, 預熱中 = 3
-        @json_url = "http://hstar-hi.alicdn.com/dream/ajax/getProjectList.htm?page=4&pageSize=20&projectType=&type=6&status=&sort=&callback=jsonp"
+        @page_json_url = "http://hstar-hi.alicdn.com/dream/ajax/getProjectList.htm?page=4&pageSize=20&projectType=&type=6&status=&sort=&callback=jsonp"
         @url = "http://hi.taobao.com/market/hi/detail2014.php?id="
       end
 
-      def get_project_links(status = 1)
-        jsons = get_total_jsons(status)
+      def get_project_links(status = "online")
+        status_code = get_status_code(status)
+        jsons = get_total_jsons(status_code)
         links = []
 
         jsons.each do |json|
@@ -20,6 +23,19 @@ module CrowdFundingParser
           links << project_url
         end
         links
+      end
+
+      def get_status_code(status)
+        case status
+        when "online"
+          1
+        when "preparing"
+          3
+        when "finished"
+          2
+        else
+          1
+        end
       end
 
       def get_total_jsons(status = 1)
@@ -37,7 +53,7 @@ module CrowdFundingParser
         page_count = get_total_page(status)
         total_urls = []
         page_count.to_i.times do |i|
-          total_urls << get_workable_json_api(i + 1, status)
+          total_urls << get_projects_page_api(i + 1, status)
         end
         total_urls
       end
@@ -48,65 +64,69 @@ module CrowdFundingParser
         page_count = json["pageTotal"]
       end
 
-      def get_workable_json_api(page = 1, status = 1)
+      private
+
+      def get_project_api(project_id)
+        "http://hstar-hi.alicdn.com/dream/ajax/getProjectForDetail.htm?id=#{project_id}"
+      end
+
+      def get_projects_page_api(page = 1, status = 1)
         "http://hstar-hi.alicdn.com/dream/ajax/getProjectList.htm?page=#{page}&pageSize=20&projectType=&type=6&status=#{status}"
       end
 
-      private
-
-      def get_json_through_url(project_url)
-        open_url = open(project_url)
-        json = JSON.load(open_url)
+      def get_id(project_url)
+        project_url.split("id=").last
       end
 
       def get_title(doc)
-        get_string(doc.css(".project-title", "h1"))
+        get_string(doc.css(".project-title h1"))
       end
 
       def get_category(doc)
-        doc.css(".page-title-wrapper").css(".pageDes").first.css("a").first.text
+        
       end
 
       def get_creator_name(doc)
-        doc.css(".page-title-wrapper").css(".pageDes")[1].css("a").first.text.strip
+        get_string(doc.css("span.sponsor-name"))
       end
 
       def get_creator_id(doc)
-        doc.css(".page-title-wrapper").css(".pageDes")[1].css("a").first["href"].split("/").last
+        # doc.css(".page-title-wrapper").css(".pageDes")[1].css("a").first["href"].split("/").last
       end
 
       def get_creator_link(doc)
-        @url + doc.css(".profilemeta .imp a").first["href"]
+        # @url + doc.css(".profilemeta .imp a").first["href"]
       end
 
       def get_summary(doc)
-        doc.css(".project_content").first.text.to_s[0..500].strip
+        summarize_project_content(get_string(doc.css("#J_Desc")))
       end
 
       # for tracking
 
       def get_money_goal(doc)
-        money_string(get_string(doc.css(".countdes .dt .white")))
+        money_string(get_string(doc.css(".target-money em")))
       end
 
       def get_money_pledged(doc)
-        money_string(get_string(doc.css(".countdes .ut .rtt h3")))
+        money_string(get_string(doc.css(".current-money")))
       end
 
       def get_backer_count(doc)
-        get_string(doc.css(".countdes .dt .pull-right")).sub("人贊助", "")
+        get_string(doc.css(".projects-schedule-data .data-number:nth-child(1)"))
       end
 
       def get_last_time(doc)
-        get_string(doc.css(".countdes .dt div:nth-child(2)")).sub("剩餘", "")
+        get_string(doc.css(".projects-schedule-data .data-number:nth-child(2)"))
       end
 
-      def get_status(status)
-        if status.match("筹款中")
+      def get_status(doc)
+        status = get_string(doc.css("a.reserve-btn.J_ReserveBtn"))
+        if status.match("我要支持")
           "online"
-        elsif status.match("制作中") || status.match("项目成功")
+        elsif status.match("筹款结束") || status.match("项目成功")
           "finished"
-        elsif status.match("预热中")
+        elsif status.match("我喜欢")
           "preparing"
         else
           "online"
@@ -114,11 +134,11 @@ module CrowdFundingParser
       end
 
       def get_fb_count(doc)
-        get_string(doc.css("#fbBtn .sharenumber"))
+        
       end
 
       def get_following_count(doc)
-        get_string(doc.css(".sidebarprj h5")).sub("人追踨", "").sub("追蹤", "").strip
+        get_string(doc.css("span.J_LikeNum"))
       end
 
       def get_backer_list(project_url)
