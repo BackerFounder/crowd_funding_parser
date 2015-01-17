d56ff068a4b1dff016e7f7627e9be1728104ef8b
Stan Low <a22301613@yahoo.com.tw>
2015-01-17 22:43:50 +0800

    now can parse project data from json

--- lib/crowd_funding_parser/parser/kickstarter.rb
+++ lib/crowd_funding_parser/parser/kickstarter.rb
@@ -4,21 +4,31 @@ require "iconv"
 
 module CrowdFundingParser
   module Parser
-    class Taobao < General
+    class Kickstarter < General
       def initialize
         @parse_method = :json
-        @url = "http://hi.taobao.com/market/hi/detail2014.php?id="
+        @jsons = []
+      end
+
+      def get_result(project_url)
+        result = []
+        Parallel.each(@jsons, in_precesses: 2, in_threads: 5) do |json|
+          result << json if json["urls"]["web"]["project"] == project_url
+        end
+        result.first
       end
 
       def get_project_links(status = "online")
         status_code = get_status_code(status)
-        jsons = get_total_jsons(status_code)
+        @jsons = get_total_jsons
+        @jsons.flatten!
+        @jsons.compact!
         links = []
-
-        Parallel.each(jsons, in_precesses: 2, in_threads: 5) do |json|
-          project_id = json["id"].to_s
-          project_url = @url + project_id
-          links << project_url
+        Parallel.each(@jsons, in_precesses: 2, in_threads: 5) do |json|
+          if json["state"] == status_code
+            project_url = json["urls"]["web"]["project"]
+            links << project_url
+          end
         end
         links
       end
@@ -26,50 +36,36 @@ module CrowdFundingParser
       def get_status_code(status)
         case status
         when "online"
-          1
+          "live"
         when "preparing"
           3
         when "finished"
-          2
+          "successful"
         else
           1
         end
       end
 
-      def get_total_jsons(status = 1)
-        urls = get_total_json_apis(status)
+      def get_total_jsons
         jsons = []
-        urls.each do |url|
-          page_json = get_json_through_url(url)
-          json = page_json["data"]
-          jsons += json
+
+        Parallel.each(1..210, in_precesses: 2, in_threads: 5) do |i|
+          begin
+            api_url = get_projects_page_api(i)
+            json = get_json_through_url(api_url)["projects"]
+            jsons << json
+          rescue Exception => e
+            puts e
+            return false
+          end
         end
         jsons
       end
 
-      def get_total_json_apis(status = 1)
-        page_count = get_total_page(status)
-        total_urls = []
-        page_count.to_i.times do |i|
-          total_urls << get_projects_page_api(i + 1, status)
-        end
-        total_urls
-      end
-
-      def get_total_page(status = 1)
-        url = "http://hstar-hi.alicdn.com/dream/ajax/getProjectList.htm?page=1&pageSize=20&projectType=&type=6&status=#{status}"
-        json = get_json_through_url(url)
-        page_count = json["pageTotal"]
-      end
-
       private
 
-      def get_project_api(project_id)
-        "http://hstar-hi.alicdn.com/dream/ajax/getProjectForDetail.htm?id=#{project_id}"
-      end
-
-      def get_projects_page_api(page = 1, status = 1)
-        "http://hstar-hi.alicdn.com/dream/ajax/getProjectList.htm?page=#{page}&pageSize=20&projectType=&type=6&status=#{status}"
+      def get_projects_page_api(page = 1)
+        "https://www.kickstarter.com/projects/search.json?search=&page=#{page}"
       end
 
       # get data info
@@ -128,13 +124,6 @@ module CrowdFundingParser
         else
           "online"
         end
-        # if result["remain_day"] == "0" && result["plan_end_days"] == ["0"]
-        #   "finished"
-        # elsif result["plan_end_days"] != ["0"]
-        #   "preparing"
-        # else
-        #   "online"
-        # end
       end
 
       def get_fb_count(result)
