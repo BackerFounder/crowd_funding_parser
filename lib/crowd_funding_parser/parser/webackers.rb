565f5513006418e82d08f9e4329cbba9b805369f
Stan Low <a22301613@yahoo.com.tw>
2015-04-19 17:21:08 +0800

    update webackers parser

--- lib/crowd_funding_parser/parser/webackers.rb
+++ lib/crowd_funding_parser/parser/webackers.rb
@@ -1,12 +1,10 @@
-require 'httparty'
-
 module CrowdFundingParser
   module Parser
     class Webackers < General
-      include HTTParty
-
-      def platform_url
-        "http://www.webackers.com"
+      def initialize
+        @platform_url = "http://www.webackers.com"
+        @item_css_class = ".cbp-item"
+        @status_css_class = "li.timeitem"
       end
 
       def get_lists
@@ -17,102 +15,85 @@ module CrowdFundingParser
         end
       end
 
-      def item_css_class
-        ".cbp-item"
-      end
+      MethodBuilder.set_methods do
+        insert_class "Webackers"
 
-      def status_css_class
-        "li.timeitem"
-      end
-
-      # for project's info
-
-      def get_title(doc)
-        get_string(doc.css(".case_header .case_title"))
-      end
-
-      def get_category(doc)
-       get_string(doc.css(".case_header .case_category a"))
-      end
-
-      def get_creator_name(doc)
-        get_string(doc.css(".headphoto_detail .name a"))
-      end
-
-      def get_creator_id(doc)
-        doc.css(".headphoto_detail .name a").first["href"].split("/").last
-      rescue
-        ""
-      end
-
-      def get_creator_link(doc)
-        platform_url + doc.css(".headphoto_detail .name a").first["href"]
-      rescue
-        ""
-      end
-
-      def get_summary(doc)
-        doc.css(".tab-content .description").first.text.to_s[0..500].strip
-      end
-
-      def get_start_date(doc)
-        
-      end
-
-      def get_end_date(doc)
-        date_string = get_string(doc.css(".container .col-md-3 .panel-body.bg_gray_h.fa-gray_d span:nth-child(2)"))
-        Date.parse(date_string)
-      end
-
-      def get_region(doc)
-        "Taiwan"
-      end
-
-      # for tracking
-
-      def get_money_goal(doc)
-        money_string(get_string(doc.css(".money_target")))
-      end
-
-      def get_money_pledged(doc)
-        money_string(get_string(doc.css(".money_now")))
-      end
-
-      def get_backer_count(doc)
-        get_string(doc.css(".tabbable li:nth-child(3) .badge.bg_no"))
-      end
-
-      def get_last_time(doc)
-        time_string = get_string(doc.css("article:nth-child(4) .panel-body span:nth-child(2)"))
-        last_seconds = (Time.parse(time_string) - Time.now)
-        last_seconds <= 0 ? "已結束" : convert_time(last_seconds)
-      end
-
-      def get_status(last_time)
-        if last_time.match("已結束") || last_time.match("已完成")
-          "finished"
-        elsif last_time.match("開始")
-          "preparing"
-        else
-          "online"
+        set_variable do
+          @platform_url = "http://www.webackers.com"
         end
-      end
 
-      def get_fb_count(doc)
-        # get 0 because of ajax delay
-        get_string(doc.css(".fbBtn span.fb_share_count"))
-      end
+        set_method :get_title do |doc|
+          get_string(doc.css(".case_header .case_title"))
+        end
 
-      def get_following_count(doc)
-        get_string(doc.css(".badge_share"))
-      end
+        set_method :get_category do |doc|
+          get_string(doc.css(".case_header .case_category a"))
+        end
 
-      def get_backer_list(project_url)
-        []
-      end
+        set_method :get_creator_name do |doc|
+          get_string(doc.css(".headphoto_detail .name a"))
+        end
 
-      def get_currency_string(result)
-        "twd"
+        set_method :get_creator_id do |doc|
+          doc.css(".headphoto_detail .name a").first["href"].split("/").last
+        end
+
+        set_method :get_creator_link do |doc|
+          @platform_url + doc.css(".headphoto_detail .name a").first["href"]
+        end
+
+        set_method :get_summary do |doc|
+          doc.css(".tab-content .description").first.text.to_s[0..500].strip
+        end
+
+        set_method :get_end_date do |doc|
+          date_string = get_string(doc.css(".container .col-md-3 .panel-body.bg_gray_h.fa-gray_d span:nth-child(2)"))
+          Date.parse(date_string)
+        end
+
+        set_method :get_region do |doc|
+          "Taiwan"
+        end
+
+        set_method :get_money_goal do |doc|
+          money_string(get_string(doc.css(".money_target")))
+        end
+
+        set_method :get_money_pledged do |doc|
+          money_string(get_string(doc.css(".money_now")))
+        end
+
+        set_method :get_backer_count do |doc|
+          get_string(doc.css(".tabbable li:nth-child(3) .badge.bg_no"))
+        end
+
+        set_method :get_last_time do |doc|
+          time_string = get_string(doc.css("article:nth-child(4) .panel-body span:nth-child(2)"))
+          last_seconds = (Time.parse(time_string) - Time.now)
+          last_seconds <= 0 ? "已結束" : convert_time(last_seconds)
+        end
+
+        set_method :get_status do |last_time|
+          if last_time.match("已結束") || last_time.match("已完成")
+            "finished"
+          elsif last_time.match("開始")
+            "preparing"
+          else
+            "online"
+          end
+        end
+
+        set_method :get_fb_count do |doc|
+          get_string(doc.css(".fbBtn span.fb_share_count"))
+        end
+
+        set_method :get_following_count do |doc|
+          get_string(doc.css(".badge_share"))
+        end
+
+        set_method :get_currency_string do |result|
+          "twd"
+        end
       end
     end
   end
