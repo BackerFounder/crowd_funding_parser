ccc6ea7b11d77233d22733a0f373e68317f8330e
Stan Low <a22301613@yahoo.com.tw>
2015-05-04 17:35:36 +0800

    update travis setting

--- spec/parsers/flyingv_spec.rb
+++ spec/parsers/flyingv_spec.rb
@@ -3,7 +3,7 @@ require 'spec_helper'
 describe CrowdFundingParser::Parser::Flyingv do
   let(:doc) { get_project_doc("https://www.flyingv.cc/project/41") }
 
-  it 'does stuff' do
+  it 'get project title' do
     expect(subject.get_title(doc)).to eq("ch+u：超電能飛行腕錶")
   end
 end
