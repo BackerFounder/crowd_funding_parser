51b8c73dfb40bd90f1cf85be89aee3cc95b9b2a2
Stan Low <a22301613@yahoo.com.tw>
2015-05-05 00:26:49 +0800

    set vcr's re-record interval

--- spec/support/vcr_sites.rb
+++ spec/support/vcr_sites.rb
@@ -1,6 +1,6 @@
 module VcrSites
  def get_project_doc(project_url, platform_name)
-   VCR.use_cassette(platform_name) do
+   VCR.use_cassette(platform_name, re_record_interval: 3.days) do
      response = HTTParty.get(project_url)
      Nokogiri::HTML(response)
    end
